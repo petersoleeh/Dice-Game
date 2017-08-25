31db8a04744839555f0998c06e5f7813e7f32562
peter soleeh <petersoleeh@gmail.com>
2017-08-25 13:07:05 +0300

    html edits structuring html file

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var die = Math.floor(Math.random()*6 +1);
+var score;
+if (die === 1) {
+  score = 0;
+  console.log("your rolled "+die+" for a score of " +score)
+} else {
+  score = die;
+  console.log("your rolled "+die+" for a score of " +score)
+}
